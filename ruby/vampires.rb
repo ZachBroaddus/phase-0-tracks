1323fbe9bcf3b83636326ad7aa48554a56c26107
Zach Broaddus <zach.broaddus@gmail.com>
2017-07-22 21:17:27 -0700

    Update/clean up vampire detection logic.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,17 +11,38 @@ insurance_preference = gets.chomp
 
 current_year = Time.new.year
 vampire_status = ""
+status_determined = false
 
-if (employee_age == current_year - employee_birth_year || employee_age == current_year - employee_birth_year - 1) && (garlic_preference == "y" || insurance_preference == "y") && (employee_name != "Drake Cula" && employee_name != "Tu Fang")
+
+# Employee gets age right and wants garlic bread, health insurance, or both.
+if (employee_age == current_year - employee_birth_year || employee_age == current_year - employee_birth_year - 1) && (garlic_preference == "y" || insurance_preference == "y")
 	vampire_status = "Probably not a vampire."
-elsif (employee_age != current_year - employee_birth_year && employee_age != current_year - employee_birth_year - 1) && ((garlic_preference == "n" && insurance_preference == "y") || (garlic_preference == "y" && insurance_preference == "n")) && (employee_name != "Drake Cula" && employee_name != "Tu Fang")
+	status_determined = true
+end
+
+# Employee gets age wrong and doesn't want either garlic bread, health insurance, or both.
+if (employee_age != current_year - employee_birth_year && employee_age != current_year - employee_birth_year - 1) && (garlic_preference == "n" || insurance_preference == "n")
     vampire_status = "Probably a vampire."
-elsif (employee_age != current_year - employee_birth_year && employee_age != current_year - employee_birth_year - 1) && (garlic_preference == "n" && insurance_preference == "n") && (employee_name != "Drake Cula" && employee_name != "Tu Fang")
+	status_determined = true
+end
+
+# Employee gets age wrong and rejects both garlic bread and health insurance.
+if (employee_age != current_year - employee_birth_year && employee_age != current_year - employee_birth_year - 1) && (garlic_preference == "n" && insurance_preference == "n")
 	vampire_status = "Almost certainly a vampire."
-elsif (employee_age == current_year - employee_birth_year || employee_age == current_year - employee_birth_year - 1 || employee_age != current_year - employee_birth_year || employee_age != current_year - employee_birth_year - 1) && (garlic_preference == "y" || garlic_preference == "n") && (insurance_preference == "y" || insurance_preference == "n") && (employee_name == "Drake Cula" || employee_name == "Tu Fang")
+	status_determined = true
+end
+
+# Employee name is either Drake Cula or Tu Fang.
+if (employee_name == "Drake Cula" || employee_name == "Tu Fang")
 	vampire_status = "Definitely a vampire."
+	status_determined = true
+end
+
+# If 'status_determined' flag has been set to true (Any of the above conditions evaluated to true), 'vampire_status' has been assigned a value and can be printed.
+if status_determined == true
+    puts vampire_status
 else
 	puts "Results inconclusive."
 end
 
-puts vampire_status
+
