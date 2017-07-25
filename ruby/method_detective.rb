fdd2773c5d395b2bba0a1a23671a92031035a78d
Zach Broaddus <zach.broaddus@gmail.com>
2017-07-25 16:22:12 -0700

    Use Ruby documentation to find string class methods to perform specific tasks

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,41 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".squeeze
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
+#Also can use "enhance".rjust(11), save new string to variable and run .ljust(11) on the new string or variable
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".gsub(/[ ]/, 'the usual ')
+# Also can use " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1..38)
+# Also can use "The mystery of the missing first letter".sub(/[T]/, '')
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".each_codepoint.map {|c| print c, ' ' }
+# Also, "z".codepoints and "z".bytes return => 122 122
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the unicode codepoint for z
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
