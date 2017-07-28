720b19195f051b93b55d809b2b0b04030aab7ca2
Zach Broaddus <zach.broaddus@gmail.com>
2017-07-27 18:40:35 -0700

    Add bubble sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,12 +1,5 @@
 # Release 0:
 def search_array(array, number)
-    # Attempt 1: if statement search
-    # array.each {|v|
-    #   if array[v] == number
-    #       search_result = array[number]
-    #   else
-    #       search_result = nil
-    #   end}
     search_result = Array.new
     count = 0
     while count < array.length
@@ -19,7 +12,9 @@ def search_array(array, number)
     end
 end
 array = [23, 543, 24, 2, 9, 17]
-p search_array(array, 3)
+puts search_array(array, 9)
+puts " "
+
 
 # Release 1:
 def fib_generator(number)
@@ -42,3 +37,27 @@ def fib_generator(number)
 end
 
 puts fib_generator(100)
+puts " "
+
+# Release 2: Bubble Sort (ascending order)
+test_array = [51, 14, 9, 2, 87, 62, 17, 102, 204, 73, 100, 281]
+def bubble_sort(array)
+    counter = 0
+    loop do
+        swapped = false
+        while counter < (array.length) -1
+            if array[counter] > array[counter+1]
+                array[counter], array[counter+1] = array[counter+1], array[counter]
+                swapped = true
+            end
+            counter += 1
+        end
+        if swapped == false
+            break
+        end
+        bubble_sort(array)
+    end
+    return array
+end
+puts "Original Array: #{test_array}"
+puts "Sorted Array: #{bubble_sort(test_array)}"
