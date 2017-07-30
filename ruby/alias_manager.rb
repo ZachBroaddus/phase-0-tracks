cf999eccb97469c7f01cf370640b2c1b99d526f2
Zach Broaddus <zach.broaddus@gmail.com>
2017-07-30 14:44:17 -0700

    Define method that detects consonants in name and converts them to next consonant

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,8 +17,13 @@ def vowel_converter(string)
 # Inner loop that loops through list of vowels, compares them with name, and changes vowels in name
         	while vowel_counter < vowel.length
         		if name_array[counter] == vowel[vowel_counter]
-        		   name_array[counter] = vowel[vowel_counter + 1]
-        		   break
+                    if name_array[counter] == 'u'
+                    	name_array[counter] = 'a'
+                        break
+                    else	
+        		        name_array[counter] = vowel[vowel_counter + 1]
+        		        break
+        		    end
         	    end
                 vowel_counter += 1
             end
@@ -27,9 +32,36 @@ def vowel_converter(string)
         end 
     return name_array
 end
+
 # Change all consonants to next consonant in alphabet
 def consonant_converter(string)
-
+    name_array = string
+    consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+    counter = 0
+    consonant_counter = 0
+# Outer loop that loops through entire name (first and last)
+        while counter < name_array.length
+# Inner loop that loops through list of consonants, compares them with name, and changes consonants in name
+        	while consonant_counter < consonant.length
+        		if name_array[counter] == consonant[consonant_counter]
+                    if name_array[counter] == 'z'
+                    	name_array[counter] = 'b'
+                        break
+                    else	
+        		        name_array[counter] = consonant[consonant_counter + 1]
+        		        break
+        		    end
+        	    end
+                consonant_counter += 1
+            end
+            counter += 1
+            consonant_counter = 0
+        end 
+    new_name = name_array.join('')
+    new_name = new_name.split(' ')
+    new_name.map! { |word| word.capitalize! }
+    new_name = new_name.join(' ')
+    return new_name
 end
 
 # Add user interface
@@ -37,4 +69,5 @@ puts "Please enter your real name:"
 real_name = gets.chomp.downcase
 
 name_reverser(real_name)
-p vowel_converter(name_reverser(real_name))
+vowel_converter(name_reverser(real_name))
+consonant_converter(vowel_converter(name_reverser(real_name)))
