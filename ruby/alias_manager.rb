a1ee6619fab292051e7ef928283c11dad974c3b6
Zach Broaddus <zach.broaddus@gmail.com>
2017-07-30 05:11:52 -0700

    Define method that converts vowel to next vowel in list

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,36 +2,39 @@
 # Reverses name
 def name_reverser(string)
 # Swap first and last name
-   name_array = string.split(' ').split('').reverse
+   name_array = string.split(' ').reverse
+   modified_name = name_array.join(' ')
 end
 
 # Change all vowels to next vowel in "aeiou"
-def vowel_converter(array)
-	new_array = []
-    array.map! { |name| 
-        new_array.push(name.split(''))
-    }
+def vowel_converter(string)
+	name_array = string.split('')
     vowel = ['a', 'e', 'i', 'o', 'u']
     counter = 0
     vowel_counter = 0
-    # Outer loop that loops through entire name (first and last)
-        # while counter < length of first and last name
-        #     Inner loop that loops through list of vowels, compares them with name, and changes vowels in name
-        # 	  while vowel_counter < 4
-        #         vowel_counter += 1
-        #     end
-        #     counter += 1
-        #     vowel_counter = 0
-        # end 
-    return new_array
+# Outer loop that loops through entire name (first and last)
+        while counter < name_array.length
+# Inner loop that loops through list of vowels, compares them with name, and changes vowels in name
+        	while vowel_counter < vowel.length
+        		if name_array[counter] == vowel[vowel_counter]
+        		   name_array[counter] = vowel[vowel_counter + 1]
+        		   break
+        	    end
+                vowel_counter += 1
+            end
+            counter += 1
+            vowel_counter = 0
+        end 
+    return name_array
 end
 # Change all consonants to next consonant in alphabet
-def consonant_converter(array)
+def consonant_converter(string)
+
 end
 
 # Add user interface
 puts "Please enter your real name:"
 real_name = gets.chomp.downcase
 
-p name_reverser(real_name)
+name_reverser(real_name)
 p vowel_converter(name_reverser(real_name))
