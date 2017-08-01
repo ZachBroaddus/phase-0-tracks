b5b7d1cabaf902e7ab024bf23a99b7f067d164eb
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-01 13:35:20 -0700

    Add methods to puppy class and create instance of puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(barks)
+    barks.times{puts "Woof! "}
+    barks
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_age)
+    human_age * 7
+  end
+
+  def drool
+    puts "*created a puddle of drool*"
+  end
+
 end
 
+rover = Puppy.new
+rover.fetch("bone")
+rover.speak(5)
+rover.roll_over
+p rover.dog_years(10)
+rover.drool
