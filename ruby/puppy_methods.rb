165da2631220d50ba0ddde4ea0a94193e88c8e44
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-01 14:08:19 -0700

    Create new class and add instances

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+# Releases 0-1
 class Puppy
 
   def fetch(toy)
@@ -33,3 +34,33 @@ rover.speak(5)
 rover.roll_over
 p rover.dog_years(10)
 rover.drool
+
+# Release 2
+class T_rex
+
+  def initialize
+    puts "Wrangling a new T-Rex..."
+  end
+
+  def snarl
+    puts "ROOOOOAAAAAARRR!!"
+  end
+
+  def rend_flesh
+    puts "*Eats an avocado whole!*"
+  end
+
+end
+
+steve = T_rex.new
+
+steve.snarl
+steve.rend_flesh
+
+T_rex_array = []
+50.times do 
+	steve = T_rex.new 
+	T_rex_array << steve
+end
+
+puts T_rex_array.length
