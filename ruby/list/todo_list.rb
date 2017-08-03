56213f05bcfa2872559786a719afd644e8e395f4
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-03 16:30:38 -0700

    Add get_item and delete_item methods and verify that they are working with rspec

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,25 +1,31 @@
 
 class TodoList
 
-def initialize(list)
-	puts "Initializing a new list..."
-    @list = list
-end
+	def initialize(list)
+		puts "Initializing a new list..."
+	    @list = list
+	end
 
 	def get_items
 		@list
 	end
 
-	def add_item(mop)
-		@list << mop
-		
+	def add_item(list_item)
+		@list << list_item
+		@list
 	end
-	def del_item
-		
+
+	def get_item(list_item)
+		@list[list_item]
 	end
-	def get_item
-		
+
+	def delete_item(list_item)
+		@list.delete(list_item)
+		@list
 	end
 end
 
-list = TodoList.new(["do the dishes", "mow the lawn"])
+# list = TodoList.new(["do the dishes", "mow the lawn"])
+# p list.add_item("mop")
+# p list.delete_item("do the dishes")
+# p list.get_item(0)
