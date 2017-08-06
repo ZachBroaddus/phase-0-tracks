e20046a4c079720c66f9022f78144f4660af9c90
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-06 15:32:32 -0700

    Create program to generate 100 santas with randomly selected attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,20 +2,20 @@ class Santa
 	attr_reader :ethnicity
 	attr_accessor :age, :gender, :reindeer_ranking
 
-	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
-	end
-
-	def eat_milk_and_cookies(cookie_type)
-		puts "That was a good #{cookie_type} cookie!"
-	end
-
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type} cookie!"
 	end
 
 # Setter methods that were removed after refactoring. Left in as comments to show that Release 2 was completed.
@@ -38,7 +38,8 @@ class Santa
 	# end
 end
 
-francine = Santa.new("female", "French")
+# Driver code used to test program
+# francine = Santa.new("female", "French")
 # francine.speak
 # francine.eat_milk_and_cookies("oatmeal raisin")
 # francine.celebrate_birthday
@@ -47,12 +48,20 @@ francine = Santa.new("female", "French")
 # puts francine.age
 # puts francine.ethnicity
 
-santa_array = []
+# Santa initializations. Left in to show that Release 1 was completed.
+# santa_array = []
 example_genders = ["transgender", "female", "bigender", "male", "gender queer", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Korean", "prefer not to say", "Shark", "N/A"]
-example_genders.length.times do |index_num|
-  santa_array << Santa.new(example_genders[index_num], example_ethnicities[index_num])
-  p santa_array[index_num]
-end
+# example_genders.length.times do |index_num|
+#   santa_array << Santa.new(example_genders[index_num], example_ethnicities[index_num])
+#   p santa_array[index_num]
+# end
 
+# Release 4
+100.times do ||
+	santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+	@age = rand(0..140)
+	puts "Santa Stats---Age: #{@age}, Gender: #{santa.gender}, Ethnicity: #{santa.ethnicity}"
+	puts " "
+end
 
