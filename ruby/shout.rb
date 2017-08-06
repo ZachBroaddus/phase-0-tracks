75ccbac64bfbd11fa6881e2fc651585fc7920326
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-06 16:47:15 -0700

    Change Shout to a mixin module and test using instances of Singer and Auctioneer classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,48 @@
+# module Shout
+#     def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+#     end
+
+#     def self.yelling_happily(words)
+#     	words + " Happy!" * 2 + " Joy!" * 2
+#     end
+# end
+
 module Shout
-    def self.yell_angrily(words)
+    def yell_angrily(words)
     	words + "!!!" + " :("
     end
 
-    def self.yelling_happily(words)
+    def yelling_happily(words)
     	words + " Happy!" * 2 + " Joy!" * 2
     end
 end
 
-puts Shout.yell_angrily("Oh no, NOT AGAIN")
-puts Shout.yelling_happily("Yasssss")
+class Singer
+	include Shout
+
+	def initialize
+		puts "Initializing a new singer..."
+	end
+end
+
+class Auctioneer
+	include Shout
+
+	def initialize
+		puts "Initializing a new auctioneer..."
+	end
+end
+
+# puts Shout.yell_angrily("Oh no, NOT AGAIN")
+# puts Shout.yelling_happily("Yasssss")
+singer = Singer.new
+puts singer.yell_angrily("Gen'rals gathered in their masses,
+Just like witches at black masses")
+puts singer.yelling_happily("I'm walking on sunshine")
+puts " "
+
+auctioneer = Auctioneer.new
+puts auctioneer.yell_angrily("Please sit down, sir")
+puts auctioneer.yelling_happily("SOLD to the man in the pink Crocs")
+puts " "
