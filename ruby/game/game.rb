62d486edf50c4a2155218a59af47017d94137c5e
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-07 00:52:49 -0700

    Refactor and clean up comments

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,14 +1,14 @@
 # Take a word from player 1
-# Give player 2 a number of guesses that is equal to the length of the word in letters
+# Give player 2 a number of guesses that is equal to the length of the word in letters, plus a few
 # Take a guess from player 2, in the form of a letter or a word if they would like to guess the word outright
-# Allow the user to solve the puzzle at any time by guessing the word outright
 # Compare the user's guess to the word entered by player 1
-# Subtract from their number of remaining tries if they guess incorrectly
-# Record the user's guess history
-# If the user makes the same guess twice, do not count it against their total number of guesses
+# If they guess the entire word correctly, end the game
 # Each time a user makes a guess, reveal to the user whether the word contained their guess letter and the position(s) of their
 # guess letter in the word
 # All other letters in the word will be hidden from the user, but displayed as blank spaces
+# Subtract from their number of remaining tries if they guess incorrectly, but
+# if the user makes the same guess twice, do not count it against their total number of guesses
+# Record the user's guess history
 # Allow the user to make guesses until they run out of tries or until they guess the word correctly
 # Display a congratulatory message if the player wins or a taunting message if they lose
 
@@ -16,12 +16,7 @@
 class WordGame
 	attr_reader :tries_remaining, :won_game, :hidden_word
 
-	attr_reader :guess_history
-
 	def initialize(original_word)
-# Input: word entered by player 1, set to variable
-# Also initialize partially revealed word to array of blank spaces, guess history log to empty array,
-# number of tries remaining according to length of player 1 word, and won_game to false
 		@original_word = original_word
 		@original_word_array = Array.new
 		@hidden_word = Array.new
@@ -32,9 +27,6 @@ class WordGame
 	end
 
 	def letter_match(guess)
-# Compare player 2 guess to player 1 word and update hidden word to reveal letters that the player guesses correctly
-# Input: player 2 guess
-# Output: updated partially revealed word
 			@original_word_array = @original_word.split("")
 			index_num = 0
 			@original_word_array.each do 
@@ -47,15 +39,10 @@ class WordGame
 	end
 
 	def update_guess_log(guess)
-# Add guess to guess history log
-# Input: player 2 guess
 		@guess_history.push(guess)
 	end
 
 	def repeat_guess(guess)
-# Check to see if guess is a repeat and decrement from total tries if it is not
-# Input: player 2 guess
-# Output: updated guess counter
 		used_a_try = true
 		@guess_history.each do |letter|
 			if letter == guess
@@ -79,9 +66,6 @@ class WordGame
 	end
 
 	def parting_message(won_game)
-# Return winning or losing message
-# Input: won game?
-# Output: parting message
 		parting_message = ""
 		if won_game
 			parting_message = "Congratulations, you won!"
@@ -92,16 +76,8 @@ class WordGame
 	end
 end
 
-# Driver test code
-# guess = "n"
-# game = WordGame.new("unicorn")
-# p game.letter_match(guess)
-# p game.repeat_guess(guess)
-# p game.update_guess_log(guess)
-# p game.won_game(guess)
-# puts game.parting_message(@won_game)
-
 # User interface
+puts " "
 puts "Welcome to Word Guess!"
 puts "Player 1, please enter a word that your opponent must guess:"
 original_word = gets.chomp.downcase
