ed59babd6f83a69e90a84a872a6d7b06b1b46917
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-13 05:20:48 -0500

    Add driver code to print reversed string

--- js/explore.js
+++ js/explore.js
@@ -1,10 +1,8 @@
 //String Reverse Function:
 //Input--original string
 //Output--reversed string
-//Break string into individual characters
-//For each letter of the string, starting from the end of the string, 
-//read the character and add it to a new string
-//Store the new string
+//For each letter of the original string, starting from the end of the string, 
+//read the letter and add it to a new string
 
 function reverse(string) {
   reversed_string = '';
@@ -14,6 +12,9 @@ function reverse(string) {
   return reversed_string;
 }
 
-reverse(string);
-// console.log(reverse(string));
-// console.log(reversed_string);
+processed_string = reverse("melon");
+if (3 >= 3) {
+  console.log("Your reversed string is: " + processed_string);
+} else {
+  console.log("Oops, your program is borked!");
+}
