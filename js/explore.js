3659aba74a5aa6ef250b68e085f7b8f115378e9b
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-13 04:34:33 -0500

    Pseudocode string reverse function

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+//String Reverse Function:
+//Get string from user
+//Break string into individual characters
+//For each letter of the string, starting from the end of the string, 
+//read the character and add it to a new string
+//Store the new string
+
