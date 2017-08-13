82b46d665a9e2c33d98f472d00c13f42b9ea2a14
Zach Broaddus <zach.broaddus@gmail.com>
2017-08-13 05:04:11 -0500

    Build string reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,19 @@
 //String Reverse Function:
-//Get string from user
+//Input--original string
+//Output--reversed string
 //Break string into individual characters
 //For each letter of the string, starting from the end of the string, 
 //read the character and add it to a new string
 //Store the new string
 
+function reverse(string) {
+  reversed_string = '';
+  for (var i = string.length - 1; i >= 0; i--) {
+    reversed_string += string[i];
+  }
+  return reversed_string;
+}
+
+reverse(string);
+// console.log(reverse(string));
+// console.log(reversed_string);
